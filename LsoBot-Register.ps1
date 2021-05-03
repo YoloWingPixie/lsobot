c91a5da27f0bd41ba270926772874ff3a93a7429
YoloWingPixie <83574853+YoloWingPixie@users.noreply.github.com>
2021-05-03 02:31:13 -0600

    Update LsoBot-Register.ps1
    Updated registration period for scheduled job from 1 to 10 years.

--- LsoBot-Register.ps1
+++ LsoBot-Register.ps1
@@ -7,7 +7,7 @@ if ([System.Diagnostics.EventLog]::SourceExists("LSO") -eq $False) {
 } 
 $start = Get-Date
 $O = New-ScheduledJobOption -MultipleInstancePolicy Queue
-$T = New-JobTrigger -Once -At $start -RepetitionInterval (New-TimeSpan -Seconds 60) -RepetitionDuration (New-Timespan -Days 365)
+$T = New-JobTrigger -Once -At $start -RepetitionInterval (New-TimeSpan -Seconds 60) -RepetitionDuration (New-Timespan -Days 3650)
 Register-ScheduledJob -Name "LSO Check" -FilePath $filePath -ScheduledJobOption $O -Trigger $T
 Write-EventLog -LogName "Application" -Source "LSO Bot" -EventId 1 -EntryType Information -Message "LSO Bot has started" -Category 1
 Write-Host "LSO Bot Powershell Scheduled Job Created"
