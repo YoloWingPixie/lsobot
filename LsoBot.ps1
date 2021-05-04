ffa18deeb5659f93a39500571173924919b482b7
YoloWingPixie <83574853+YoloWingPixie@users.noreply.github.com>
2021-05-04 10:19:54 -0600

    Update LsoBot.ps1
    Additional regex bug fixes to prevent Discord webhook spams
    
    First attempt at rewriting the LSO message to something saner.

--- LsoBot.ps1
+++ LsoBot.ps1
@@ -30,7 +30,7 @@ if ($landingEvent -eq $null ) {
 
 # Strip the log message down to the time that the log event occurred. 
 $logTime = $landingEvent
-$logTime = $logTime -replace "^.*(?:dcs\.log\:\d{5}\:)", ""
+$logTime = $logTime -replace "^.*(?:dcs\.log\:\d{1,5}\:)", ""
 $logTime = $logTime -replace "\..*$", ""
 #$logTime = $logTime.split()[-1]
 
@@ -46,9 +46,260 @@ $diff = New-TimeSpan -Start $trapTime -End $sysTime
 
 $Grade = $landingEvent
 $Grade = $Grade -replace "^.*(?:comment=LSO:)", ""
-
 $Grade = $Grade -replace ",.*$", ""
 
+<# 
+---------------------------------------------------------------------
+                        BEGIN REGRADING
+---------------------------------------------------------------------
+#>
+
+#Grade Regex
+$rGRADE =    "GRADE:\S{1,3}"
+$1WIRE =     "(?:WIRE# 1)"
+$PERFECT =  "GRADE:_OK_"
+$OK =       "GRADE:OK"
+$FAIR =     "GRADE:(OK)"
+$NOGRADE =  "GRADE:---"
+$CUT =      "GRADE:C"
+$WO =       "GRADE:WO"
+$OWO =      "GRADE:OWO"
+$WOAFU =    "WO\(AFU\)(TL|IC|AR)"
+
+#Grade Remarks Regex - Removals
+$SLOX = "(_|\()?(?:SLOX)(_|\))?"
+$EGIW = "(_|\()?(?:EGIW)(_|\))?"
+$BC = "(?:\[BC\])"
+
+# Left and Right positions, no minor deviations
+$LEFT = "(?!\))_?D?L?U?L(X|IM|IC|AR)_?(?!\))"
+$RIGHT = "(?!\))_?D?L?U?R(X|IM|IC|AR)_?(?!\))"
+
+
+# (X) At Start
+$LULX =     "(_|\()?(?:LULX)(_|\))?"
+$LURX =     "(_|\()?(?:LURX)(\)|_)?"
+$HX =       "(_|\()?(?:HX)(_|\))?"
+$LOX =      "(_|\()?(?:LOX)(_|\))?"
+$FX =       "(_|\()?(?:FX)(_|\))?"
+$NX =       "(_|\()?(?:NX)(_|\))?"
+$WX =       "(_|\()?(?:WX)(_|\))?"
+$DRX =      "(_|\()?(?:DRX)(_|\))?"
+$DLX =      "(_|\()?(?:DLX)(_|\))?"
+
+# (IM) In Middle
+$LURIM =    "(_|\()?(?:LURIM)(_|\))?"
+$LULIM =    "(_|\()?(?:LULIM)(_|\))?"
+$HIM =      "(_|\()?(?:HIM)(_|\))?"
+$LOIM =     "(_|\()?(?:LOIM)(_|\))?"
+$DRIM =     "(_|\()?(?:DRIM)(_|\))?"
+$DLIM =     "(_|\()?(?:DLIM)(_|\))?"
+$FIM =      "(_|\()?(?:FIM)(_|\))?"
+$SLOIM =    "(_|\()?(?:SLOIM)(_|\))?"
+$WIM =      "(_|\()?(?:WIM)(_|\))?"
+$TMRDIM =   "(_|\()?(?:TMRDIM)(_|\))?"
+$NERDIM =   "(_|\()?(?:NERDIM)(_|\))?"
+
+# (IC) In Close
+$LURIC =    "(_|\()?(?:LURIC)(_|\))?"
+$LULIC =    "(_|\()?(?:LULIC)(_|\))?"
+$LOIC =     "(_|\()?(?:LOIC)(_|\))?"
+$HIC =      "(_|\()?(?:HIC)(_|\))?"
+$FIC =      "(_|\()?(?:FIC)(_|\))?"
+$PIC =      "(_|\()?(?:PIC)(_|\))?"
+$PPPIC =    "(_|\()?(?:PPPIC)(_|\))?"
+$WIC =      "(_|\()?(?:WIC)(_|\))?"
+$DRIC =     "(_|\()?(?:DRIC)(_|\))?"
+$DLIC =     "(_|\()?(?:DLIC)(_|\))?"
+$NERDIC =   "(_|\()?(?:NERDIC)(_|\))?"
+$TMRDIC =   "(_|\()?(?:TMRDIC)(_|\))?"
+$SLOIC =    "(_|\()?(?:SLOIC)(_|\))?"
+
+
+# (AR) At Ramp
+$LURAR =    "(_|\()?(?:LURAR)(_|\))?"
+$LULAR =    "(_|\()?(?:LULAR)(_|\))?"
+$LOAR =     "(_|\()?(?:LOAR)(_|\))?"
+$HAR =      "(_|\()?(?:HAR)(_|\))?"
+$FAR =      "(_|\()?(?:FAR)(_|\))?"
+$SLOAR =    "(_|\()?(?:SLOAR)(_|\))?"
+$PAR =      "(_|\()?(?:PAR)(_|\))?"
+$WAR =      "(_|\()?(?:WAR)(_|\))?"
+$DRAR =     "(_|\()?(?:DRAR)(_|\))?"
+$DLAR =     "(_|\()?(?:DLAR)(_|\))?"
+$NERDAR =   "(_|\()?(?:NERDAR)(_|\))?"
+$TMRDAR =   "(_|\()?(?:TMRDAR)(_|\))?"
+
+# (IW) In Wires
+$LURIW =    "(_|\()?(?:LURIW)(_|\))?"
+$LULIW =    "(_|\()?(?:LULIW)(_|\))?"
+$LOIW =     "(_|\()?(?:LOIW)(_|\))?"
+$SLOIW =    "(_|\()?(?:SLOIW)(_|\))?"
+$FIW =      "(_|\()?(?:FIW)(_|\))?"
+$LLIW =     "(_|\()?(?:LLIW)(_|\))?"
+$LRIW =     "(_|\()?(?:LRIW)(_|\))?"
+$3PTSIW =   "(_|\()?(?:3PTSIW)(_|\))?"
+$BIW =      "(_|\()?(?:BIW)(_|\))?"
+$EGTL =     "(_|\()?(?:EGTL)(_|\))?"
+
+
+# Remove SLOX, EGIW, and BC from vocab
+if ($Grade -match $SLOX ) {
+    $Grade = $Grade -replace $SLOX, ""
+    $Grade = $Grade -replace '\s+', ' '
+    }
+if ($Grade -match $EGIW) {
+    $Grade = $Grade -replace $EGIW, ""
+    $Grade = $Grade -replace '\s+', ' '
+    }
+if ($Grade -match $BC) {
+    $Grade = $Grade -replace $BC, ""
+    $Grade = $Grade -replace '\s+', ' '
+    }
+
+    $lockGrade = 0
+
+#Find instances where DRX\DLX and LURX\LULX are called together, and replace with simply LURX\LULX
+if ((($Grade -match $DRX) -and ($Grade -match $LURX)) -or (($Grade -match $DLX) -and ($Grade -match $LULX))) {
+    $Grade = $Grade -replace $DRX, ""
+    $Grade = $Grade -replace $DLX, ""
+    $Grade = $Grade -replace '\s+', ' '
+
+}
+
+#Check for waveoffs
+if (($Grade -match $WO) -or ($Grade -match $OWO) -or ($Grade -match $WOAFU)) {
+    $lockGrade = 1
+}
+
+# Check for automatic Cuts
+if ($lockGrade -eq 0) {
+    if (($Grade -match $LLIW) -or 
+        ($Grade -match $LRIW) -or 
+        ($Grade -match $SLOIC) -or 
+        ($Grade -match $SLOAR) -or 
+        ($Grade -match $SLOIW) -or
+        ($Grade -match $PPPIC)) {
+
+            $Grade = $Grade -replace $rGRADE, $CUT
+            $Grade = $Grade -replace '\s+', ' '
+            $lockGrade = 1
+    }
+}
+
+# Check for TMRDIC or TMRDAR and EGTL or 3PTS for a cut pass OR if TMRDIC or TMRDAR were major deviations
+
+if ($lockGrade -eq 0) {
+    if ((($Grade -match $TMRDIC) -or ($Grade -match $TMRDAR)) -and (($Grade -match $EGTL) -or ($Grade -match $3PTSIW)) ) {
+        $Grade = $Grade -replace $rGRADE, $CUT
+        $Grade = $Grade -replace '\s+', ' '
+        $lockGrade = 1
+    }
+    elseif ($Grade -match "_TMRD(IC|AR)_") {
+        $Grade = $Grade -replace $rGRADE, $CUT
+        $Grade = $Grade -replace '\s+', ' '
+        $lockGrade = 1
+    }
+    
+}
+
+# Check for No Grades
+if ($lockGrade -eq 0) {
+    if (($Grade -match $TMRDAR) -or
+        ($Grade -match $TMRDIC) -or
+        ($Grade -match $3PTSIW) -or 
+        ($Grade -match $BIW) -or 
+        ($Grade -match $EGTL) -or 
+        ($Grade -match $TMRDIM) -or 
+        ($Grade -match $SLOIM) -or 
+        ($Grade -match $PPPIC) -or 
+        ($Grade -match $DRIC) -or 
+        ($Grade -match $DLIC) -or 
+        ($Grade -match $LULIC) -or 
+        ($Grade -match $LURIC) -or 
+        ($Grade -match $NERDIC) -or 
+        ($Grade -match $DRAR) -or 
+        ($Grade -match $DLAR) -or 
+        ($Grade -match $NERDAR) -or 
+        ($Grade -match $LURAR) -or 
+        ($Grade -match $LULAR) -or 
+        ($Grade -match $WAR) -or 
+        ($Grade -match $FIW)) {
+
+            $Grade = $Grade -replace $rGRADE, $NOGRADE
+            $Grade = $Grade -replace '\s+', ' '
+            $lockGrade = 1
+    }
+}
+
+#Check for oscillating flight paths and No Grade
+if ($lockGrade -eq 0) {
+    if (($Grade -match $LEFT) -and ($Grade -match $RIGHT)) {
+        $Grade = $Grade -replace $rGRADE, $NOGRADE
+        $Grade = $Grade -replace '\s+', ' '
+        $lockGrade = 1        
+    }
+}
+
+
+# Check for fair passes
+if ($lockGrade -eq 0) {
+    if (($Grade -match $DRX) -or 
+    ($Grade -match $DLX) -or 
+    ($Grade -match $DRIM) -or 
+    ($Grade -match $DLIM) -or 
+    ($Grade -match $LURIM) -or 
+    ($Grade -match $LULIM) -or 
+    ($Grade -match $NERDIM) -or 
+    ($Grade -match $FIM) -or 
+    ($Grade -match $WIM) -or 
+    ($Grade -match $FIC) -or 
+    ($Grade -match $HIC) -or 
+    ($Grade -match $LOIC) -or 
+    ($Grade -match $PIC) -or 
+    ($Grade -match $WIC) -or 
+    ($Grade -match $HAR) -or 
+    ($Grade -match $FAR)) {
+
+        $Grade = $Grade -replace $rGRADE, $FAIR
+        $Grade = $Grade -replace '\s+', ' '
+        $lockGrade = 1
+    }
+}
+
+# Check for OK passes
+if ($lockGrade -eq 0) {
+    if (($Grade -match $LULX) -or 
+        ($Grade -match $LURX) -or 
+        ($Grade -match $FX) -or 
+        ($Grade -match $HX) -or 
+        ($Grade -match $HIM) -or 
+        ($Grade -match $NX) -or 
+        ($Grade -match $WX)) {
+
+            $Grade = $Grade -replace $rGRADE, $OK
+            $Grade = $Grade -replace '\s+', ' '
+            $lockGrade = 1
+    }
+}
+
+# Check for empty #3 wires and change to _OK_
+if ($Grade -match "GRADE:\S{1,4}\s*?:\s*WIRE#\s*3") {
+    $Grade = $Grade -replace $rGRADE, $PERFECT
+}
+# Check for empty #2 and #4 wires and switch to OK
+if ($Grade -match "GRADE:\S{1,4}\s*?:\s*WIRE#\s*(2|4)") {
+    $Grade = $Grade -replace $rGRADE, $OK
+}
+
+<# 
+---------------------------------------------------------------------
+                        END REGRADING
+---------------------------------------------------------------------
+#>
+
+
+$Grade = $Grade -replace '\s+', ' '
 $Grade = $Grade -replace "_", "\_"
 
 #Strip the log message down to the pilot name
